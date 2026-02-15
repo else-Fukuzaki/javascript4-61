695164681feba7c32ef17a22c25e16c4c095f466
else-Fukuzaki <i@else.ws>
2026-02-15 16:05:15 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+let count = 0;
+// console.log(count);
+count = 30;
+// console.log(count);
+let newCount = 0;
+const daysInWeek = 7;
+let tomatoCount;
+let $tomato$Count;
+let _tomato_Count;
+let tomato7Count7;
+
+
+let additionResult = 2 + 5;
+let result = 5;
+result = result + 1;
+result += 1;
+// console.log(++result);
+result++;
+++result;
+
+let number = 10; //integer
+number = -4; //integer
+number = 3.142; //float
+let string = 'Hello, World!'; //string
+number = 'hello'; //stringも代入できる
+
+const userName = 'Alice';
+string = `hello ${userName}!`; //テンプレートリテラル
+// console.log(string);
+
+let userInfo = null;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+const returnedValue = add(2, 3);
+console.log(returnedValue);
